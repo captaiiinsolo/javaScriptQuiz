9dc33792a4e612a791f49331899fd0ed722f6144
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-02 03:40:02 -0800

    add jquery script. hide questions when quiz ends. add try again button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,109 +1,94 @@
 // Document Selectors
 var startButton = document.querySelector("#startQuizButton");
+var tryAgainButton = document.querySelector("#tryAgain");
 var quizTimer = document.querySelector("#timer");
 var highscores = document.querySelector("#highscores");
 var quizSection = document.querySelector("#quizContainer");
+var quizTitle = document.querySelector(".quizTitle");
+var quizInstructions = document.querySelector(".quizInstructions");
+var questionNumber = document.querySelector(".questionNumber");
+var questionText = document.querySelector(".questionText");
+var questionOptions = document.querySelector(".questionOptions");
 
 // Global Variables
 var timeRemaining;
 var penalty;
 var reward;
 var timerInterval;
+var userScore;
+var userInitials;
 
-// Displays Timer on page
-// quizTimer.textContent = "Time: " + timeRemaining;
 
+
+
+
+// Quiz timer starts on button click
 function startQuizTimer() {
         // Makes sure multiple instances are not allowed.
         clearInterval(timerInterval); 
+
+        // Initial time for quiz. Increase start time by to account for delay in display.
         timeRemaining = 6;
+
+        // Countdown interval function
         timerInterval = setInterval(function() {
         timeRemaining--;
         quizTimer.textContent = "Time: " + timeRemaining;
-        startButton.style.visibility = "hidden"; // Hides start quiz button so quiz cannot be restarted until time runs out
-
+        quizInstructions.style.visibility = "hidden";
+        startButton.style.display = "none"; // Hides start quiz button so quiz cannot be restarted until time runs out
         
+
+        // When timer ends 
         if(timeRemaining === 0) {
             clearInterval(timerInterval);
             quizTimer.textContent = "TIME'S UP!";
-            startButton.innerHTML = "Try Again?" // Changes startButton text to Try Again
-            startButton.style.visibility = "visible"; // Brings back startButton
+            tryAgainButton.style.display = "";
+            // remove quiz questions from page
+            $("#quizContainer").empty();
+
+            
         }
     }, 1000 * 1);
 }
 
 // Quiz Questions, Options, and Answers
 var quizQuestions = [
-    {
-        questionNumb: 1,
-        question:"Is this question number 1?",
-        options: [{
-            a: 'no',
-            b: 'maybe',
-            c: 'yes',
-            d: 'idk',
-        }],
-        answer:"c"
-    },
-
-    {
-        questionNumb: 2,
-        question:"Is this question number 2?",
-        options: [{
-            a: 'no',
-            b: 'maybe',
-            c: 'yes',
-            d: 'idk',
-        }],
-        answer:"c"
-    },
-
-    {
-        questionNumb: 3,
-        question:"Is this question number 3?",
-        options: [{
-            a: 'no',
-            b: 'maybe',
-            c: 'yes',
-            d: 'idk',
-        }],
-        answer:"c"
-    },
-
-    {
-        questionNumb: 4,
-        question:"Is this question number 4?",
-        options: [{
-            a: 'no',
-            b: 'maybe',
-            c: 'yes',
-            d: 'idk',
-        }],
-        answer:"c"
-    },
-
-    {
-        questionNumb: 5,
-        question:"Is this question number 5?",
-        options: [{
-            a: 'no',
-            b: 'maybe',
-            c: 'yes',
-            d: 'idk',
-        }],
-        answer:"c"
-    }
-
+    ["Question: 1" + "\n", 
+    {"question": "Is this question 1?" + "\n",
+        "options": ["a: Yes" + "\n", ["b: no" + "\n"], ["c: Idk" + "\n"], ["d: Maybe"]]
+    }]
 ];
 
-function iterateQuizQuestions(arrOfQuestions) {
-for (var i = 0; i < arrOfQuestions.length; i++) {
-console.log("Question #: " + arrOfQuestions[i].questionNumb);
-console.log(arrOfQuestions[i].question);
-console.log(arrOfQuestions[i].options[0]);
-    }
+
+function hideTryAgain() {
+    tryAgainButton.style.display = "none";
 }
 
-iterateQuizQuestions(quizQuestions);
+
+function startQuiz() {
+    $(questionNumber).append(quizQuestions[0][0]);
+    $(questionText).append(quizQuestions[0][1].question);
+    $(questionOptions).append(quizQuestions[0][1].options[0] + quizQuestions[0][1].options[1] + quizQuestions[0][1].options[2] + quizQuestions[0][1].options[3]);
+
+}
+
+// Console log question title, question, and options
+console.log(quizQuestions[0][0]);
+
+console.log(quizQuestions[0][1].question);
+
+console.log(quizQuestions[0][1].options[0] + quizQuestions[0][1].options[1][0] + quizQuestions[0][1].options[2][0] + quizQuestions[0][1].options[3][0]);
 
 startButton.addEventListener("click", startQuizTimer);
+
+startButton.addEventListener("click", startQuiz);
+
+
+// Create function that builds homepage so quiz can be rerun from beginning upon clicking try again button
+window.onload = function buildPage() {
+highscores.textContent = "View Highscores";
+quizTitle.textContent = "JavaScript Coding Quiz Challenge";
+quizInstructions.textContent = "Try to answer the following JavaScript related questions within the time limit. Keep in mind that all incorrect answers will penalize your score/time by 15 seconds.";
+hideTryAgain();
+
+}
