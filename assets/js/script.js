9d44eb68ddcb2f57dcff770cc469c51ac25ef449
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-01 23:55:33 -0800

    update timer and quiz button features. iterate through test questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,24 +4,32 @@ var quizTimer = document.querySelector("#timer");
 var highscores = document.querySelector("#highscores");
 var quizSection = document.querySelector("#quizContainer");
 
-// Declared Variables
-var timeRemaining = 30;
-var penalty = 15;
-var reward = 20;
+// Global Variables
+var timeRemaining;
+var penalty;
+var reward;
+var timerInterval;
 
 // Displays Timer on page
-quizTimer.textContent = "Time: " + timeRemaining;
+// quizTimer.textContent = "Time: " + timeRemaining;
 
 function startQuizTimer() {
-    var timerInterval = setInterval(function() {
+        // Makes sure multiple instances are not allowed.
+        clearInterval(timerInterval); 
+        timeRemaining = 6;
+        timerInterval = setInterval(function() {
         timeRemaining--;
         quizTimer.textContent = "Time: " + timeRemaining;
+        startButton.style.visibility = "hidden"; // Hides start quiz button so quiz cannot be restarted until time runs out
+
         
-        if(timeRemaining === 0 || timeRemaining <=0) {
+        if(timeRemaining === 0) {
             clearInterval(timerInterval);
             quizTimer.textContent = "TIME'S UP!";
+            startButton.innerHTML = "Try Again?" // Changes startButton text to Try Again
+            startButton.style.visibility = "visible"; // Brings back startButton
         }
-    }, 1000);
+    }, 1000 * 1);
 }
 
 // Quiz Questions, Options, and Answers
@@ -38,6 +46,64 @@ var quizQuestions = [
         answer:"c"
     },
 
+    {
+        questionNumb: 2,
+        question:"Is this question number 2?",
+        options: [{
+            a: 'no',
+            b: 'maybe',
+            c: 'yes',
+            d: 'idk',
+        }],
+        answer:"c"
+    },
+
+    {
+        questionNumb: 3,
+        question:"Is this question number 3?",
+        options: [{
+            a: 'no',
+            b: 'maybe',
+            c: 'yes',
+            d: 'idk',
+        }],
+        answer:"c"
+    },
+
+    {
+        questionNumb: 4,
+        question:"Is this question number 4?",
+        options: [{
+            a: 'no',
+            b: 'maybe',
+            c: 'yes',
+            d: 'idk',
+        }],
+        answer:"c"
+    },
+
+    {
+        questionNumb: 5,
+        question:"Is this question number 5?",
+        options: [{
+            a: 'no',
+            b: 'maybe',
+            c: 'yes',
+            d: 'idk',
+        }],
+        answer:"c"
+    }
+
 ];
+
+function iterateQuizQuestions(arrOfQuestions) {
+for (var i = 0; i < arrOfQuestions.length; i++) {
+console.log("Question #: " + arrOfQuestions[i].questionNumb);
+console.log(arrOfQuestions[i].question);
+console.log(arrOfQuestions[i].options[0]);
+    }
+}
+
+iterateQuizQuestions(quizQuestions);
 
 startButton.addEventListener("click", startQuizTimer);
