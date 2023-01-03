f9d5605ee230b3703b6189f5a0376e7434b92672
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-02 19:18:07 -0800

    create new page to house quiz questions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -22,10 +22,7 @@ var userScore;
 var userInitials;
 
 
-
-
-
-// Quiz timer starts on button click
+// Starts quiz timer
 function startQuizTimer() {
         // Makes sure multiple instances are not allowed.
         clearInterval(timerInterval); 
@@ -37,18 +34,12 @@ function startQuizTimer() {
         timerInterval = setInterval(function() {
         timeRemaining--;
         quizTimer.textContent = "Time: " + timeRemaining;
-        quizInstructions.style.visibility = "hidden";
-        startButton.style.display = "none"; // Hides start quiz button so quiz cannot be restarted until time runs out
         
 
         // When timer ends 
         if(timeRemaining === 0) {
             clearInterval(timerInterval);
             quizTimer.textContent = "TIME'S UP!";
-            tryAgainButton.style.display = "";
-            // remove quiz questions from page
-            $("#quizContainer").empty();
-
             
         }
     }, 1000 * 1);
@@ -62,13 +53,6 @@ var quizQuestions = [
     }]
 ];
 
-
-function hideTryAgain() {
-    tryAgainButton.style.display = "none";
-}
-
-
-
 // Console log question title, question, and options
 console.log(quizQuestions[0][0]);
 
@@ -76,21 +60,4 @@ console.log(quizQuestions[0][1].question);
 
 console.log(quizQuestions[0][1].options[0] + quizQuestions[0][1].options[1][0] + quizQuestions[0][1].options[2][0] + quizQuestions[0][1].options[3][0]);
 
-startButton.addEventListener("click", startQuizTimer);
-
-questionText.append(quizQuestions[0][0] + " " + quizQuestions[0][1].question);
-choiceA.append(quizQuestions[0][1].options[0]);
-choiceB.append(quizQuestions[0][1].options[1]);
-choiceC.append(quizQuestions[0][1].options[2]);
-choiceD.append(quizQuestions[0][1].options[3]);
-
-// Create function that builds homepage so quiz can be rerun from beginning upon clicking try again button
-window.onload = function buildPage() {
-highscores.textContent = "View Highscores";
-quizTitle.textContent = "JavaScript Coding Quiz Challenge";
-quizInstructions.textContent = "Try to answer the following JavaScript related questions within the time limit. Keep in mind that all incorrect answers will penalize your score/time by 15 seconds.";
-hideTryAgain();
-
-}
-
-window.onload;
+startQuizTimer();
